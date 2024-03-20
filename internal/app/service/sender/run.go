8ee95fe70b7ce4da5355ad1c09191ab60620b746
Nikita Denisenok <nikitadenisenok@gmail.com>
2024-03-20 18:26:34 +0300

    fixed issue with model in sender

--- internal/app/service/sender/run.go
+++ internal/app/service/sender/run.go
@@ -34,7 +34,7 @@ func (s *Service) Run(ctx context.Context) {
 		case msg := <-msgChan:
 			err = s.receiveBookings(msg)
 			if err != nil {
-				log.Error("could not receive messages: ", sl.Err(err))
+				log.Error("could not receive message: ", sl.Err(err))
 			}
 			err = msg.Ack(false)
 			if err != nil {
@@ -55,33 +55,31 @@ func (s *Service) receiveBookings(msg amqp.Delivery) error {
 
 	log.Info(fmt.Sprintf("Received a message: %s", msg.Body))
 
-	var bookings []*model.BookingInfo
-	err := json.Unmarshal(msg.Body, &bookings)
+	var booking *model.BookingInfo
+	err := json.Unmarshal(msg.Body, &booking)
 	if err != nil {
 		log.Error("failed to unmarshal message", sl.Err(err))
 		return err
 	}
 
-	for _, booking := range bookings {
-		log.Info(fmt.Sprintf(
-			"Booking:  %d \n "+
-				"SuiteID: %d \n "+
-				"StartDate: %v \n "+
-				"EndDate: :%v \n "+
-				"NotifyAt: %v \n "+
-				"OwnerID: %d \n "+
-				"CreatedAt: %v \n "+
-				"UpdatedAt: %v \n\n ",
-			booking.ID,
-			booking.SuiteID,
-			booking.StartDate,
-			booking.EndDate,
-			booking.NotifyAt,
-			booking.UserID,
-			booking.CreatedAt,
-			booking.UpdatedAt,
-		))
-	}
+	log.Info(fmt.Sprintf(
+		"Booking:  %d \n "+
+			"SuiteID: %d \n "+
+			"StartDate: %v \n "+
+			"EndDate: :%v \n "+
+			"NotifyAt: %v \n "+
+			"OwnerID: %d \n "+
+			"CreatedAt: %v \n "+
+			"UpdatedAt: %v \n\n ",
+		booking.ID,
+		booking.SuiteID,
+		booking.StartDate,
+		booking.EndDate,
+		booking.NotifyAt,
+		booking.UserID,
+		booking.CreatedAt,
+		booking.UpdatedAt,
+	))
 
 	return nil
 }
